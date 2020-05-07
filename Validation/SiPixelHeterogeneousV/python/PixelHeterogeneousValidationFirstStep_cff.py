b634a436c5062f4db73738225c092eebc87c9e2f
Suvankar Roy Chowdhury <suvankar.roy.chowdhury@cern.ch>
2020-05-07 02:13:36 +0200

    for dpg workshop

---
+++ Validation/SiPixelHeterogeneousV/python/PixelHeterogeneousValidationFirstStep_cff.py
@@ -0,0 +1,13 @@
+import FWCore.ParameterSet.Config as cms
+#import the validation modules
+##modules that import SoA directly from GPU
+from Validation.SiPixelHeterogeneousV.pixelValidateRecHitGPU_cfi import *
+
+##modules that import intermediate SoA from CPU
+from Validation.SiPixelHeterogeneousV.pixelValidateVerticesFromSoA_cfi import *
+
+#Add the modules in the Sequence
+#the following sequence needs to be added to the standard validation sequence##TODO
+pixelHeterogeneousValidationSource = cms.Sequence(sipixelrechitvalid)
+
+pixelHeterogeneousValidationFromsoaSource = cms.Sequence(sipixelrechitvalid)
