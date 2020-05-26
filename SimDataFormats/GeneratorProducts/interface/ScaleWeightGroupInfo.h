d81c38c28a3c7afe9fa47598c890ce90f810b11e
Dylan Teague <dteague@cern.ch>
2020-05-26 11:32:33 +0200

    Simplify ScaleWeight structure in anticipation of dyn weight changes

--- SimDataFormats/GeneratorProducts/interface/ScaleWeightGroupInfo.h
+++ SimDataFormats/GeneratorProducts/interface/ScaleWeightGroupInfo.h
@@ -3,40 +3,27 @@
 
 #include "SimDataFormats/GeneratorProducts/interface/WeightGroupInfo.h"
 #include <unordered_map>
+#include <vector>
 
 namespace gen {
   class ScaleWeightGroupInfo : public WeightGroupInfo {
   private:
     bool isFunctionalFormVar_;
-    size_t centralIndex_;
-    size_t muR1muF2Index_;
-    size_t muR1muF05Index_;
-    size_t muR2muF05Index_;
-    size_t muR2muF1Index_;
-    size_t muR2muF2Index_;
-    size_t muR05muF05Index_;
-    size_t muR05muF1Index_;
-    size_t muR05muF2Index_;
+    std::vector<size_t> muIndices_;
+
     // Dyn_scale
     // bool hasDyn = false;
     // Maybe have mapping of [muR##muF##index] = vector<dyn_scale_indices>
     // std::unordered_map<size_t, std::vector<size_t>> dynMap;
+    void setMuRMuFIndex(WeightMetaInfo& info, float muR, float muF);
+    inline int getIndex(int muR, int muF) const { return 3 * muR + muF; }
+    inline bool isValidValue(float mu) const { return mu == 0.5 || mu == 1.0 || mu == 2.0; }
 
   public:
     ScaleWeightGroupInfo() : ScaleWeightGroupInfo("") {}
-    ScaleWeightGroupInfo(std::string header, std::string name) : WeightGroupInfo(header, name) {
+    ScaleWeightGroupInfo(std::string header, std::string name) : WeightGroupInfo(header, name), muIndices_(9) {
       weightType_ = WeightType::kScaleWeights;
       isFunctionalFormVar_ = false;
-      centralIndex_ = 0;
-      muR1muF2Index_ = 0;
-      muR1muF05Index_ = 0;
-      muR2muF05Index_ = 0;
-      muR2muF1Index_ = 0;
-      muR2muF2Index_ = 0;
-      muR2muF05Index_ = 0;
-      muR05muF05Index_ = 0;
-      muR05muF1Index_ = 0;
-      muR05muF2Index_ = 0;
     }
     ScaleWeightGroupInfo(std::string header) : ScaleWeightGroupInfo(header, header) {}
     ScaleWeightGroupInfo(const ScaleWeightGroupInfo& other) { copy(other); }
@@ -44,22 +31,21 @@ namespace gen {
     void copy(const ScaleWeightGroupInfo& other);
     virtual ScaleWeightGroupInfo* clone() const override;
 
-    void setMuRMuFIndex(WeightMetaInfo& info, float muR, float muF);
     void setMuRMuFIndex(int globalIndex, std::string id, float muR, float muF);
     void addContainedId(int weightEntry, std::string id, std::string label, float muR, float muF);
 
     // Is a variation of the functional form of the dynamic scale
     bool isFunctionalFormVariation();
     void setIsFunctionalFormVariation(bool functionalVar) { isFunctionalFormVar_ = functionalVar; }
-    size_t centralIndex() const { return centralIndex_; }
-    size_t muR1muF2Index() const { return muR1muF2Index_; }
-    size_t muR1muF05Index() const { return muR1muF05Index_; }
-    size_t muR2muF05Index() const { return muR2muF05Index_; }
-    size_t muR2muF1Index() const { return muR2muF1Index_; }
-    size_t muR2muF2Index() const { return muR2muF2Index_; }
-    size_t muR05muF05Index() const { return muR05muF05Index_; }
-    size_t muR05muF1Index() const { return muR05muF1Index_; }
-    size_t muR05muF2Index() const { return muR05muF2Index_; }
+    size_t centralIndex() const { return muIndices_.at(4); }
+    size_t muR1muF2Index() const { return muIndices_.at(5); }
+    size_t muR1muF05Index() const { return muIndices_.at(3); }
+    size_t muR2muF05Index() const { return muIndices_.at(6); }
+    size_t muR2muF1Index() const { return muIndices_.at(7); }
+    size_t muR2muF2Index() const { return muIndices_.at(8); }
+    size_t muR05muF05Index() const { return muIndices_.at(0); }
+    size_t muR05muF1Index() const { return muIndices_.at(1); }
+    size_t muR05muF2Index() const { return muIndices_.at(2); }
   };
 }  // namespace gen
 
--- SimDataFormats/GeneratorProducts/src/ScaleWeightGroupInfo.cc
+++ SimDataFormats/GeneratorProducts/src/ScaleWeightGroupInfo.cc
@@ -4,15 +4,7 @@
 
 namespace gen {
   void ScaleWeightGroupInfo::copy(const ScaleWeightGroupInfo& other) {
-    centralIndex_ = other.centralIndex_;
-    muR1muF2Index_ = other.muR1muF2Index_;
-    muR1muF05Index_ = other.muR1muF05Index_;
-    muR2muF05Index_ = other.muR2muF05Index_;
-    muR2muF1Index_ = other.muR2muF1Index_;
-    muR2muF2Index_ = other.muR2muF2Index_;
-    muR05muF05Index_ = other.muR2muF05Index_;
-    muR05muF1Index_ = other.muR05muF1Index_;
-    muR05muF2Index_ = other.muR05muF2Index_;
+    muIndices_ = other.muIndices_;
     WeightGroupInfo::copy(other);
   }
 
@@ -29,25 +21,10 @@ namespace gen {
   }
 
   void ScaleWeightGroupInfo::setMuRMuFIndex(WeightMetaInfo& info, float muR, float muF) {
-    if (muR == 0.5 && muF == 0.5)
-      muR05muF05Index_ = info.localIndex;
-    else if (muR == 0.5 && muF == 1.0)
-      muR05muF1Index_ = info.localIndex;
-    else if (muR == 0.5 && muF == 2.0)
-      muR05muF2Index_ = info.localIndex;
-    else if (muR == 1.0 && muF == 0.5)
-      muR1muF05Index_ = info.localIndex;
-    else if (muR == 1.0 && muF == 1.0)
-      centralIndex_ = info.localIndex;
-    else if (muR == 1.0 && muF == 2.0)
-      muR1muF2Index_ = info.localIndex;
-    else if (muR == 2.0 && muF == 0.5)
-      muR2muF05Index_ = info.localIndex;
-    else if (muR == 2.0 && muF == 1.0)
-      muR2muF1Index_ = info.localIndex;
-    else if (muR == 2.0 && muF == 2.0)
-      muR2muF2Index_ = info.localIndex;
-    else
+    int index = getIndex(muR, muF);
+    if (index < 0 || index > 8 || !(isValidValue(muR) && isValidValue(muF))) {
       isWellFormed_ = false;
+    }
+    muIndices_[index] = info.localIndex;
   }
 }  // namespace gen
