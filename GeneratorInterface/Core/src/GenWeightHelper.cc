801700a7f54d8cac036951c386be0803b3772b5e
Kenneth Long <kdlong@hep.wisc.edu>
2020-08-04 01:47:34 +0200

    Use boost trim

--- GeneratorInterface/Core/src/GenWeightHelper.cc
+++ GeneratorInterface/Core/src/GenWeightHelper.cc
@@ -52,10 +52,4 @@ namespace gen {
     buildGroups();
     printWeights();
   }
-
-  inline std::string GenWeightHelper::trim(std::string &s) {
-    s.erase(s.begin(), std::find_if(s.begin(), s.end(), std::not1(std::ptr_fun<int, int>(std::isspace))));
-    s.erase(std::find_if(s.rbegin(), s.rend(), std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
-    return s;
-  }
 }  // namespace gen
