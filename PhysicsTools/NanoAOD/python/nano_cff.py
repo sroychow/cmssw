874f7b0bf15b0cf0d8ae33d465bae74f71240347
Kenneth Long <kdlong@hep.wisc.edu>
2022-01-16 01:54:47 +0100

    Step forward to newer cmssw version
    Fix problem of confounding issues in xmlParsing
    
    code fails to parse correctly when swapped weigthgroup tags and escaped
    &lt;/&gt; characters in header.
    
    Also clean up the xmlError parsing and add some debug statements
    associated with tose errors
    
    falling back to vector of flattables
    
    Add "fillEmptyIfWeightFails" option and fix an err
    
    Error is related to the product size not being updated when an unknown
    weight is found
    
    Add more error states to LHEWEightHelper to handle empty xml

--- PhysicsTools/NanoAOD/python/nano_cff.py
+++ PhysicsTools/NanoAOD/python/nano_cff.py
@@ -21,17 +21,8 @@ from PhysicsTools.NanoAOD.vertices_cff import *
 from PhysicsTools.NanoAOD.met_cff import *
 from PhysicsTools.NanoAOD.triggerObjects_cff import *
 from PhysicsTools.NanoAOD.isotracks_cff import *
+from PhysicsTools.NanoAOD.protons_cff import *
 from PhysicsTools.NanoAOD.NanoAODEDMEventContent_cff import *
-
-from Configuration.Eras.Modifier_run2_miniAOD_80XLegacy_cff import run2_miniAOD_80XLegacy
-from Configuration.Eras.Modifier_run2_nanoAOD_92X_cff import run2_nanoAOD_92X
-from Configuration.Eras.Modifier_run2_nanoAOD_94X2016_cff import run2_nanoAOD_94X2016
-from Configuration.Eras.Modifier_run2_nanoAOD_94XMiniAODv1_cff import run2_nanoAOD_94XMiniAODv1
-from Configuration.Eras.Modifier_run2_nanoAOD_94XMiniAODv2_cff import run2_nanoAOD_94XMiniAODv2
-from Configuration.Eras.Modifier_run2_nanoAOD_102Xv1_cff import run2_nanoAOD_102Xv1
-from Configuration.Eras.Modifier_run2_nanoAOD_106Xv1_cff import run2_nanoAOD_106Xv1
-from Configuration.Eras.Modifier_run2_egamma_2017_cff import run2_egamma_2017
-from Configuration.Eras.Modifier_run2_egamma_2018_cff import run2_egamma_2018
 
 
 nanoMetadata = cms.EDProducer("UniqueStringProducer",
@@ -118,7 +109,7 @@ nanoTableTaskFS = cms.Task(
     genWeightsTableTask, genVertexTablesTask, genParticleTablesTask, genProtonTablesTask, particleLevelTablesTask
 )
 
-nanoSequence = cms.Sequence(nanoSequenceCommon + nanoSequenceOnlyFullSim)
+nanoSequence = cms.Sequence(nanoSequenceCommon + nanoSequenceOnlyData + nanoSequenceOnlyFullSim)
 
 nanoSequenceFS = cms.Sequence(genParticleSequence + genVertexTables + particleLevelSequence + nanoSequenceCommon + jetMC + muonMC + electronMC + photonMC + tauMC + metMC + ttbarCatMCProducers +  globalTablesMC + btagWeightTable + genWeightsTable + genVertexTable + genParticleTables + particleLevelTables + lheInfoTable  + ttbarCategoryTable )
 
